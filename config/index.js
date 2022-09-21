3e6836afb16c013bbbc7da9b7b40b9a298bf9355
JMM0806 <114075125+JMM0806@users.noreply.github.com>
2022-09-21 21:09:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "omrJj6r14Wg4dVDZGGYHWVH8t3KQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "jObnOiIk9g02D4Ocx7OUwqm03hP90Bm8C7edTSw3UJc",
+      useTemplateId: "IQqioowp15EMQG3Kz1giO0y0HVBBipSu7ymWl5mwL-k",
       // 所在省份
       province: "陕西",
       // 所在城市
@@ -135,7 +135,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "jObnOiIk9g02D4Ocx7OUwqm03hP90Bm8C7edTSw3UJc",
+  CALLBACK_TEMPLATE_ID: "	IQqioowp15EMQG3Kz1giO0y0HVBBipSu7ymWl5mwL-k",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
